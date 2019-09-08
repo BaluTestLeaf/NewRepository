2ba00fe91d39f37c5cc8e159d6ece5ca20ac7329
Balumahedran <Balumahedran@DESKTOP-O8F13LJ>
2019-09-08 12:08:59 +0530

    Re-post the files

---
+++ src/main/java/org/testleaf/leaftaps/pages/HomePage.java
@@ -0,0 +1,22 @@
+package org.testleaf.leaftaps.pages;
+
+import org.testleaf.leaftaps.base.ProjectSpecificMethods;
+
+public class HomePage extends ProjectSpecificMethods {
+	
+public LoginPage clickLogout() {
+	driver.findElementByClassName("decorativeSubmit").click();
+	return new LoginPage();
+}
+
+}
+
+
+
+
+
+
+
+
+
+
